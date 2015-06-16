e2abd0f587c09b7efa2c4d3ca7a1a576e3edda0b
laixintao <799303812@163.com>
2015-06-16 14:41:58 +0800

    2.1

---
+++ 2.get-corpus-and-list/2.1-the-different-of-raw-sent-words.py
@@ -0,0 +1,12 @@
+
+import nltk;
+from nltk.corpus import gutenberg
+
+raw = gutenberg.raw("burgess-busterbrown.txt");
+print raw[1:20];
+
+words = gutenberg.words("burgess-busterbrown.txt");
+print words[1:20];
+
+sents = gutenberg.sents("burgess-busterbrown.txt");
+print sents[1:20];
