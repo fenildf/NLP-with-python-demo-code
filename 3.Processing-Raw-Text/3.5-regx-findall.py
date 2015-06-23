3ef611282d0fdba42553e8953159b980eab47ff2
laixintao <799303812@163.com>
2015-06-23 22:19:45 +0800

    find vowel and their freqdist

--- 3.Processing-Raw-Text/3.5-regx-findall.py
+++ 3.Processing-Raw-Text/3.5-regx-findall.py
@@ -5,3 +5,13 @@ word = 'supercalifragilisticexpialidocious'
 print re.findall(r'[aeiou]',word)
 
 print len(re.findall(r'[aeiou]',word))
+
+# find two or more [aeiou] and their freqdist
+
+import nltk
+
+wsj = sorted(set(nltk.corpus.treebank.words()))
+fd = nltk.FreqDist(vs for word in  wsj
+        for vs in re.findall(r'[aeiou]{2,}',word))
+
+print fd.items()
